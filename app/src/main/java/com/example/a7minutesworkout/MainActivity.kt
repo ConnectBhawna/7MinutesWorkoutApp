d33d99131d5c639f7def793e7eaa1488bf373207
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-04-12 17:20:52 +0530

    Splash Screen

---
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fLStartButton: FrameLayout = findViewById(R.id.flStart)
+        fLStartButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Here we will start the exercise.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
