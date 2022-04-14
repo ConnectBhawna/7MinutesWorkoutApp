473de0ac9320df3f8c9387a4777fb9be802cdba3
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-04-14 11:09:46 +0530

    Done with implementing the ViewBinding

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -4,19 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.example.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding:ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val fLStartButton: FrameLayout = findViewById(R.id.flStart)
-        fLStartButton.setOnClickListener {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        //val fLStartButton: FrameLayout = findViewById(R.id.flStart)
+        binding?.flStart?.setOnClickListener {
             Toast.makeText(
                 this@MainActivity,
                 "Here we will start the exercise.",
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
