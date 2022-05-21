c56a13da6edc13cfb288bf9073e1a819a81fe18d
OxVidhi <81790585+Bhawna1203@users.noreply.github.com>
2022-05-21 12:09:54 +0530

    finally Completed

--- app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a7minutesworkout.databinding.ActivityHistoryBinding
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -43,8 +44,29 @@ class HistoryActivity : AppCompatActivity() {
     private fun getAllCompletedDates(historyDao: HistoryDao) {
         lifecycleScope.launch {
             historyDao.fetchAllDates().collect { allCompletedDatesList ->
-                for (i in allCompletedDatesList){
-                    Log.e("Date: ","" + i)
+                if(allCompletedDatesList.isNotEmpty()){
+                    binding?.tvHistory?.visibility = View.VISIBLE
+                    binding?.rvHistory?.visibility = View.VISIBLE
+                    binding?.tvNoDataAvailable?.visibility = View.GONE
+
+                    // Creates a vertical Layout Manager
+                    binding?.rvHistory?.layoutManager = LinearLayoutManager(this@HistoryActivity)
+
+                    // History adapter is initialized and the list is passed in the param.
+                    val dates = ArrayList<String>()
+                    for (date in allCompletedDatesList){
+                        dates.add(date.date)
+                    }
+                    val historyAdapter = HistoryAdapter(ArrayList(dates))
+
+                    // Access the RecyclerView Adapter and load the data into it
+                    binding?.rvHistory?.adapter = historyAdapter
+                }
+                else {
+                    binding?.tvHistory?.visibility = View.GONE
+                    binding?.rvHistory?.visibility = View.GONE
+                    binding?.tvNoDataAvailable?.visibility = View.VISIBLE
+
                 }
             }
         }
